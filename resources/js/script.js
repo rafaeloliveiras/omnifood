ffa5b38b1d24a0eae0d75a47e992d363c2e26e45
Rafael Oliveira <rafael.oliveiras@live.com>
2018-08-10 00:09:29 -0300

    added animations with animate.css

--- resources/js/script.js
+++ resources/js/script.js
@@ -10,4 +10,35 @@ $(document).ready(function() {
     offset:'60px;'
   });
 
+
+  $('.js-animation').waypoint(function(direction){
+    $('.js-animation').addClass('animated fadeIn');
+  }, {
+    offset: '70%'
+  });
+
+  $('.js-animation2').waypoint(function(direction){
+    $('.js-animation2').addClass('animated slideInUp');
+  }, {
+    offset: '90%'
+  });
+
+  $('.js-animation3').waypoint(function(direction){
+    $('.js-animation3').addClass('animated shake');
+  }, {
+    offset: '40%'
+  });
+
+  $('.js-animation4').waypoint(function(direction){
+    $('.js-animation4').addClass('animated flipInX');
+  }, {
+    offset: '80%'
+  })
+
+  $('.js-animation5').waypoint(function(direction){
+    $('.js-animation5').addClass('animated fadeIn');
+  }, {
+    offset: '90%'
+  })
+
 });
